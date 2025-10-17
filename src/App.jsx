c9471648664057e2f6c6aaed10b8fa7912a26ea1
argsdio <arianneguisadio@gmail.com>
2025-10-17 16:40:28 -0400

    input agent ability

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,40 @@ import AgentIcon from './components/AgentIcon'
 function App() {
   const [count, setCount] = useState(0)
   const agentNames = ['astra', 'breach', 'brimstone', 'chamber', 'clove', 'cypher', 'deadlock', 'fade', 'gekko', 'harbor', 'iso', 'jett', 'kay-o', 'killjoy', 'neon', 'omen', 'phoenix', 'raze', 'reyna', 'sage', 'skye', 'sova', 'tejo', 'viper', 'vyse', 'waylay', 'yoru']
+  const [currentTeam, setCurrentTeam] = useState([]) //max 4 agents
+
+  const handleAgentClick = (agent) => {
+    if (currentTeam.length < 4) {
+        setCurrentTeam([...currentTeam, agent]);
+        console.log(currentTeam);
+    }
+
+  };
+
+  const handleRemoveAgent = (index) => {
+    setCurrentTeam(currentTeam.filter((_, i) => i !== index));
+  };
 
   return (
     <>
-        <div>
+        <div className="grid grid-cols-5 gap-4 mb-8">
+      {[0, 1, 2, 3, 4].map(index => (
+        <div 
+          key={index}
+          className="aspect-square bg-slate-800 flex items-center justify-center"
+          onClick={() => currentTeam[index] && handleRemoveAgent(index)}
+        >
+          {currentTeam[index] ? (
+            <AgentIcon agent={currentTeam[index]} className="w-full h-full object-contain" />
+          ) : (
+            <AgentIcon agent={null} className="w-full h-full object-contain"/>
+          )}
+        </div>
+      ))}
+    </div>
+        <div className='agent-select-table'>
             {agentNames.map((name) => (
-                <AgentIcon agent={name} />
+                <AgentIcon key={name} agent={name} onClick={() => handleAgentClick(name)} isSelected = {currentTeam.includes(name)}/>
             ))}
         </div>
     </>
--- src/components/AgentIcon.jsx
+++ src/components/AgentIcon.jsx
@@ -1,12 +1,17 @@
+import React from 'react';
+import { useState } from 'react'
 import '../Styles.css';
 
-function AgentIcon({ agent }) {
+function AgentIcon({ agent, onClick, isSelected }) {
     const id = agent
 
+    function toggleOnTeam() {
+        setOnTeam(!onTeam);
+    }
     return (
         <>
-            <div className="agent-icon">
-                <img className='icon' src={`src/icons/${id.toLowerCase()}.webp`} alt={id} />
+            <div className={isSelected ? "agent-selected-icon" : "agent-icon"} onClick = {onClick}>
+                <img className='icon' src={agent ? `src/icons/${id.toLowerCase()}.webp` : 'src/icons/qmark.png'} alt={id}/>
             </div>
         </>
     )
